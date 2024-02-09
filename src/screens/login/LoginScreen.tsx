158860d06f4bdedc7dcf5801af36a1b08a935086
jungbug <xbox1111@naver.com>
2024-02-10 03:18:26 +0900

    add: login style

--- src/screens/login/LoginScreen.tsx
+++ src/screens/login/LoginScreen.tsx
@@ -1,10 +1,8 @@
-import React, { useEffect } from "react";
-import { View , Text, Button} from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, Text, Button, TextInput, } from "react-native";
 
 import { ParamListBase } from "@react-navigation/native";
 import { StackScreenProps } from "@react-navigation/stack";
-
-import { createStackNavigator } from "@react-navigation/stack";
 
 import { NAVIGATOR_BOTTOM_TAB } from "@utils/screens";
 import { NAVIGATOR_REGISTER } from "@utils/screens";
@@ -12,6 +10,9 @@ import { NAVIGATOR_REGISTER } from "@utils/screens";
 type Props = StackScreenProps<ParamListBase, "Login">;
 
 const LoginScreen: React.FC<Props> = ({ navigation }) => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
     const handlePress = () => {
         navigation.replace(NAVIGATOR_BOTTOM_TAB);
     };
@@ -19,16 +20,47 @@ const LoginScreen: React.FC<Props> = ({ navigation }) => {
         navigation.replace(NAVIGATOR_REGISTER);
     };
 
-	return (
-        <View className="h-full w-full justify-center px-4">
-			<Text className="text-2xl font-bold text-center mt-8">
-                loginScreen
-			</Text>
-            <Button title="클릭하세요" onPress={handlePress} />
-            <Button title="회원가입" onPress={registerHandler}></Button>
-		</View>
+    return (
+        <View className="flex h-full w-full bg-white">
+            <View className="flex flex-col justify-center items-center py-12 px-4">
+                <Text className="text-2xl font-poppins font-bold mb-4 text-gray-800">
+                    로그인
+                </Text>
 
-	);
+                <TextInput
+                    className="mb-4 border border-gray-300 rounded-lg px-4 py-2 w-full focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    placeholder="이메일 주소를 입력하세요"
+                    value={email}
+                    onChangeText={setEmail}
+                />
+                <TextInput
+                    className="mb-4 border border-gray-300 rounded-lg px-4 py-2 w-full focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    placeholder="비밀번호를 입력하세요"
+                    value={password}
+                    onChangeText={setPassword}
+                    secureTextEntry
+                />
+                <View className="flex justify-end w-full mb-4 bg-indigo-500 rounded-full">
+                    <Button
+                        title="로그인"
+                        onPress={handlePress}
+                        color={"white"}
+                    />
+                </View>
+                <Text className="text-gray-500">비밀번호를 잊으셨나요?</Text>
+
+                <Text className="text-gray-500 mb-4">
+                    계정이 없으신가요?
+                    <Text
+                        className="text-blue-500 cursor-pointer"
+                        onPress={registerHandler}
+                    >
+                        회원가입
+                    </Text>
+                </Text>
+            </View>
+        </View>
+    );
 };
 
 export default LoginScreen;
