5bb2dd4c35fdfcd8b64c43d7ae36501f2c60b15c
jungbug <xbox1111@naver.com>
2024-02-09 17:27:43 +0900

    delete: start page

--- src/navigation/Navigation.tsx
+++ src/navigation/Navigation.tsx
@@ -3,7 +3,6 @@ import React from "react";
 import {NavigationContainer} from "@react-navigation/native";
 import {createStackNavigator} from "@react-navigation/stack";
 
-import LandingScreen from "@screens/LandingScreen";
 import {NAVIGATOR_BOTTOM_TAB, NAVIGATOR_LANDING} from "@utils/screens";
 
 import BottomTabNavigation from "./BottomTabNavigation";
